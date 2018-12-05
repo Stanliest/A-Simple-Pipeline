23759bb8287d9472badc369588e9fc45281c9a7e
Stanley Liu <stanley.liu@ibm.com>
2018-12-05 10:04:26 -0500

    try out first build with travis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { PageHeader } from "react-bootstrap";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <PageHeader>
+        A Simple Pipeline <small>Under development</small>
+      </PageHeader>
     );
   }
 }
